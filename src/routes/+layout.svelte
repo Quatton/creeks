8e02ed47306901a9f5592db177f8d492dd06bb57
Quatton <atomicatives@gmail.com>
2023-08-09 17:43:57 +0900

    skeleton ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,19 @@
 <script>
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+
+	import '../app.postcss';
 	import Header from './Header.svelte';
-	import './styles.css';
+	import Footer from './Footer.svelte';
 </script>
 
-<div class="app">
+<div class="flex flex-col" style="height: 100svh">
 	<Header />
 
-	<main>
+	<main class="flex-1 grid place-content-center">
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
