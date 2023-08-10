be913eb1bac0f95a00594eac11f5192defa6391e
Quatton <atomicatives@gmail.com>
2023-08-11 06:07:03 +0900

    style table

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -5,10 +5,28 @@
 		tableMapperValues,
 		type TableSource
 	} from "@skeletonlabs/skeleton";
+	import { derived } from "svelte/store";
+	import { format } from "date-fns";
+
+	const convertedSessions = derived(sessions, ($sessions) => {
+		if (!$sessions) return [];
+		return $sessions.map((session) => {
+			return {
+				...session,
+				// Wed, 21 Jul 2021 00:00
+				createdAt: format(new Date(session.createdAt), "ccc, dd MMM yyyy HH:mm")
+			};
+		});
+	});
 
 	const sessionTable: TableSource = {
-		head: ["Created At", "Title", "Content"],
-		body: tableMapperValues($sessions, ["createdAt", "title", "content"])
+		head: ["Title", "Content", "Created At"],
+		body: tableMapperValues($convertedSessions, [
+			"title",
+			"content",
+			"createdAt"
+		]),
+		meta: tableMapperValues($convertedSessions, ["id"])
 	};
 </script>
 
@@ -17,6 +35,6 @@
 	<meta name="description" content="Your saved notes" />
 </svelte:head>
 
-<section>
+<section class="container self-start">
 	<Table source={sessionTable} interactive />
 </section>
