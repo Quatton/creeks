193de6759b3085336c7537aff89891e1a4d03a8e
Quatton <atomicatives@gmail.com>
2023-08-11 22:17:06 +0900

    dark logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,18 @@
 <script>
 	import logo from "$lib/images/memoiz.svg";
+	import darkLogo from "$lib/images/memoiz-dark.svg";
 	import github from "$lib/images/github.svg";
+	import { modeCurrent } from "@skeletonlabs/skeleton";
 </script>
 
 <header class="flex justify-between">
 	<div class="w-12 h-12">
 		<a href="/" class="flex items-center justify-center w-full h-full">
-			<img src={logo} alt="SvelteKit" class="w-8 h-8 object-contain" />
+			<img
+				src={$modeCurrent ? logo : darkLogo}
+				alt="SvelteKit"
+				class="w-8 h-8 object-contain"
+			/>
 		</a>
 	</div>
 
